5e138ac3bfd8bd9c186ea661f9b4995c9e2c7c49
MehmetAydar01 <mehmet.aydar07@gmail.com>
2024-12-22 19:05:06 +0300

    3.22 alistirmasi cozuldu ve eslint config eklendi

--- index.js
+++ index.js
@@ -1,100 +1,100 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-const Person = require('./models/person');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
 
-app.use(express.static('dist'));
-app.use(cors());
-app.use(express.json());
+app.use(express.static('dist'))
+app.use(cors())
+app.use(express.json())
 
 // Özelleştirilmiş Morgan formatı
-morgan.token('body', (request, response) => JSON.stringify(request.body)); // request body'sini alır (istek gövdesini alır --> POST requestler için)
+morgan.token('body', (request) => JSON.stringify(request.body)) // request body'sini alır (istek gövdesini alır --> POST requestler için)
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
-);
+)
 
 app.get('/', (request, response) => {
-  response.send('<h1>Phonebook Project</h1>');
-});
+  response.send('<h1>Phonebook Project</h1>')
+})
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
-  });
-});
+    response.json(persons)
+  })
+})
 
-app.get('/info', (request, response) => {
-  const date = new Date();
+app.get('/info', (_request, response, next) => {
+  const date = new Date()
 
   const sendData = (peopleCount) => {
     return `<p>phonebook has info for ${
       peopleCount > 1 ? `${peopleCount} people` : `${peopleCount} person`
-    } <br /><br /> ${date}</p>`;
-  };
+    } <br /><br /> ${date}</p>`
+  }
 
   Person.countDocuments()
     .then((peopleCount) => {
-      response.send(sendData(peopleCount));
+      response.send(sendData(peopleCount))
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.post('/api/persons', (request, response, next) => {
-  const { name, number } = request.body;
+  const { name, number } = request.body
 
   if (!name || !number) {
     return response.status(400).json({
       error: 'The name or number is missing',
-    });
+    })
   }
 
   const person = new Person({
     name,
     number,
-  });
+  })
 
   // We use validateSync to control custom validator in Mongoose
-  const validationError = person.validateSync(); // This runs the validation on the schema
+  const validationError = person.validateSync() // This runs the validation on the schema
 
   if (validationError && validationError.errors['number']) {
     return response.status(400).json({
       error: validationError.errors['number'].message,
-    });
+    })
   }
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const { name, number } = request.body;
+  const { name, number } = request.body
 
   Person.findByIdAndUpdate(
     request.params.id,
@@ -102,41 +102,41 @@ app.put('/api/persons/:id', (request, response, next) => {
     { new: true, runValidators: true, context: 'query' }
   )
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // For Unknown Endpoint
 const unknownEndpoint = (request, response) => {
   response.status(404).send({
     error: 'unknown endpoint',
-  });
-};
+  })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 // For Error Handler
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
     return response.status(400).send({
       error: 'malformatted id',
-    });
+    })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({
       error: error.message,
-    });
+    })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- mongo.js
+++ mongo.js
@@ -1,24 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument');
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://phonebookapp:${password}@cluster0.ul8jf.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://phonebookapp:${password}@cluster0.ul8jf.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
 
-mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false)
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: String,
   number: String,
-});
+})
 
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model('Person', personSchema)
 
 // Creating new person in the database...
 
@@ -35,11 +35,11 @@ const Person = mongoose.model('Person', personSchema);
 // Fetching person objects from the database...
 
 Person.find({}).then((result) => {
-  console.log('phonebook:');
+  console.log('phonebook:')
 
   result.forEach((person) => {
-    console.log(person.name, person.number);
-  });
+    console.log(person.name, person.number)
+  })
 
-  mongoose.connection.close();
-});
+  mongoose.connection.close()
+})
